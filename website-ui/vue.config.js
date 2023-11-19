992d912e2f2209a1c350cc26b2c44a1e962dd7a8
Jackz <jacksonbixby@gmail.com>
2023-11-19 11:00:06 -0600

    Switch to 8081 for dev server

--- website-ui/vue.config.js
+++ website-ui/vue.config.js
@@ -3,7 +3,7 @@ module.exports = {
     devServer: {
         proxy: {
             '^/api': {
-                target: 'http://localhost:8082'
+                target: 'http://localhost:8081'
             }
         }
     },

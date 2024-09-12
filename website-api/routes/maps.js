dbf470549c1d9fc5d52f0fcba12eaddef6c40608
Jackz <me@jackz.me>
2024-09-11 20:59:41 -0500

    Use distinct campaignID for games played This avoids counting duplicates for each player

--- website-api/routes/maps.js
+++ website-api/routes/maps.js
@@ -5,7 +5,7 @@ import routeCache from 'route-cache'
 export default function(pool) {
     router.get('/', routeCache.cacheSeconds(120), async (req, res) => {
         const [rows] = await pool.query(`
-            SELECT i.mapid as map_id, i.name as map_name, AVG(r.value) as avg_rating, COUNT(DISTINCT r.steamid) as num_ratings, COUNT(g.id) as games_played, AVG(g.duration) as avg_duration, MAX(g.date_end) as last_played_time
+            SELECT i.mapid as map_id, i.name as map_name, AVG(r.value) as avg_rating, COUNT(DISTINCT r.steamid) as num_ratings, COUNT(DISTINCT g.campaignID) as games_played, AVG(g.duration) as avg_duration, MAX(g.date_end) as last_played_time
             FROM map_info i
             LEFT JOIN left4dead2.map_ratings r ON i.mapid = r.map_id
             LEFT JOIN left4dead2.stats_games g ON g.map = i.mapid

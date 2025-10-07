ffff39ad5b5e11664dcc3febf9e5bdc0034b0592
Jackz <me@jackz.me>
2025-10-06 22:14:21 -0500

    Improve js search, add html search

--- website/src/pages/api/search.ts
+++ website/src/pages/api/search.ts
@@ -2,14 +2,11 @@ import { searchUsers } from "@/models/User.ts";
 import { apiError, apiJson } from "@/utils/api.ts";
 import type { APIRoute } from "astro";
 
-const SEARCH_LIMIT = 20
-
-export const GET: APIRoute = async ({ params, request }) => {
-    const url = new URL(request.url)
+export const GET: APIRoute = async ({ params, request, url }) => {
     let query = url.searchParams.get("q")
     if(!query) return apiError(400, "BAD_REQUEST", "Missing ?q")
     query = query.toLowerCase()
 
-    const users = await searchUsers(query, SEARCH_LIMIT)
+    const users = await searchUsers(query, 6)
     return apiJson(users)
 }

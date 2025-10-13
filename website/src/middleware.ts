83c881883f6a7e4c96632b5b6fa878a20eedc61c
Jackz <me@jackz.me>
2025-10-12 20:56:50 -0500

    Add logs for CORS

--- website/src/middleware.ts
+++ website/src/middleware.ts
@@ -3,6 +3,8 @@ import { defineMiddleware } from "astro:middleware";
 const CORS_ALLOW_LIST = process.env.API_ALLOWED_ORIGINS ? process.env.API_ALLOWED_ORIGINS.split("") : [""]
 const CORS_ALLOW_ANY = CORS_ALLOW_LIST[0] === "*"
 
+console.info("[Cors] Allowed Origins:", CORS_ALLOW_ANY ? "-any-" : CORS_ALLOW_LIST.join(" "))
+
 export const metrics = {
     requests: {
         total: 0,
@@ -17,9 +19,10 @@ export const onRequest = defineMiddleware(async (context, next) => {
     if(context.url.pathname.startsWith("/api")) {
         metrics.requests.api++
         const response = await next();
+        console.debug(context.url.origin, context.site?.origin, context.url.origin)
         if(CORS_ALLOW_ANY) {
             response.headers.set("Access-Control-Allow-Origin", "*")
-        } else if(context.url.origin === context.site?.origin || CORS_ALLOW_LIST.includes(context.url.origin)) {
+        } else if((context.site && context.url.origin === context.site.origin) || CORS_ALLOW_LIST.includes(context.url.origin)) {
             response.headers.set("Access-Control-Allow-Origin", context.url.origin)
         }
         // Handle any 404 for API so we always try to return JSON
